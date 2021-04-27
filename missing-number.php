40abe4c91194f284adcb8c546d52933663c5d8bb
kingnoahkong <81228826+kingnoahkong@users.noreply.github.com>
2021-04-27 15:52:39 +0200

    Update missing-number.php

--- missing-number.php
+++ missing-number.php
@@ -13,3 +13,14 @@ function findMissingNum($A){
 
 $a = [5, 3, 1, 2];
 echo findMissingNum($a);
+
+//second variation using simple calculations and array_sum();
+function alternativeFunc($a){
+   $b = (count($a) +1) * (count($a)+2) / 2;
+   $c = $b- array_sum($a);
+
+   return $c;
+
+}
+
+echo alternativeFunc($a);
